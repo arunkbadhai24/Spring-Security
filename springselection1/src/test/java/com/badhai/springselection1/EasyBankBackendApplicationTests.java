b96241e4f40787087e4c05f295ac1fd3ae0bdd6f
arunkbadhai24 <arunkb2024@gmail.com>
2024-09-14 15:28:31 +0530

    Initial commit

---
+++ springselection1/src/test/java/com/badhai/springselection1/EasyBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badhai.springselection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springselection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
