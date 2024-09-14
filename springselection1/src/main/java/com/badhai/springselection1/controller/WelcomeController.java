8840e324018162a67ee07fb8591be244503512c1
arunkbadhai24 <arunkb2024@gmail.com>
2024-09-14 18:46:11 +0530

    Added custom Username and password

---
+++ springselection1/src/main/java/com/badhai/springselection1/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.badhai.springselection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring Application with Security";
+    }
+}
