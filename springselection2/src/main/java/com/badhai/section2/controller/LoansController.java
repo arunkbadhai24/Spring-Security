7613d582521a160ef8952076a6d89e06cd66b331
arunkbadhai24 <arunkb2024@gmail.com>
2024-09-15 19:01:27 +0530

    initial commit

---
+++ springselection2/src/main/java/com/badhai/section2/controller/LoansController.java
@@ -0,0 +1,13 @@
+package com.badhai.section2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+
+    @GetMapping("/myLoans")
+    public String getLoanDetails(){
+        return "Here are the loans details from the DB";
+    }
+}
